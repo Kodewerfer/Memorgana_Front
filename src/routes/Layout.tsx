42b22ed3ebcc600edca81149496a321689395157
Kodewerfer <lijleen@gmail.com>
2022-02-17 15:31:16 -0500

    layout basic

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,22 +1,54 @@
 import React from "react";
+import styles from "./Layout.module.css";
 import { Link, Outlet } from "react-router-dom";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { BiBrain } from "react-icons/bi";
+import { CgDarkMode } from "react-icons/cg";
 
 export default function Layout() {
   return (
     <>
-      <nav className="relative top-0 m-0 flex h-screen flex-col flex-wrap overflow-auto bg-slate-800 pr-2 text-white">
-        <div className="">
-          <Link to="/Memoboard" className="w-full truncate">
-            Memo'board
-          </Link>
-        </div>
-        <div className="">
-          <Link to="/">Memowo</Link>
-        </div>
+      <div className={styles.search}>
+        <input
+          type="text"
+          placeholder="search memos.."
+          className={`${styles.searchBar} peer`}
+        />
+        <span className={`${styles.darkMode} peer-focus:scale-0`}>
+          <CgDarkMode size={30} />
+        </span>
+      </div>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          <Items
+            to="/Memoboard"
+            icon={<AiOutlineDashboard size={50} />}
+            text={"Memo'board"}
+          />
+          <Items to="/" icon={<BiBrain size={50} />} text={"Memʘωʘ"} />
+        </ul>
       </nav>
-      <main className="overflow-y-auto pl-5">
+      <main className={styles.main}>
         <Outlet />
       </main>
     </>
+  );
+}
+
+// nav item component
+function Items({
+  to,
+  icon,
+  text = "Default Text",
+}: {
+  to: string;
+  icon: any;
+  text: string;
+}) {
+  return (
+    <div className={`${styles.item} group`}>
+      <Link to={to}>{icon}</Link>
+      <span className={`${styles.tooltip} group-hover:scale-100`}>{text}</span>
+    </div>
   );
 }
